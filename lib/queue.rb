a4fa5ae4f10f337ee46549cee2b1354a9dbc4dc4
jasmin47 <alxsanborn@gmail.com>
2016-11-08 18:40:58 -0500

    done

--- lib/queue.rb
+++ lib/queue.rb
@@ -1,5 +1,15 @@
 require_relative 'stack'
 
 class Queue
+  def initialize()
+    @@queue = []
+  end
 
+  def enqueue(num)
+    @@queue.push(num)
+  end
+
+  def dequeue
+    return @@queue.shift
+  end
 end
--- lib/stack.rb
+++ lib/stack.rb
@@ -1,3 +1,18 @@
 class Stack
+  def initialize
+    @@stack = []
+  end
+
+  def push(num)
+    @@stack.push(num)
+  end
+
+  def pop
+    return @@stack.pop
+  end
+
+  def size
+    return @@stack.size
+  end
 
 end
